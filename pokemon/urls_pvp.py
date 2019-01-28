600614ccd724f83a50cdb8d6771de2d24f3f9c1b
Brobin <tobin@bulubox.com>
2019-01-28 09:40:52 -0600

    too much processing

--- pokemon/urls_pvp.py
+++ pokemon/urls_pvp.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import BestPokemon, PvpIVSpread
+from .views import PvpIVSpread
 
 
 urlpatterns = [
-    path('best', BestPokemon.as_view(), name='pvp-best'),
     path('iv', PvpIVSpread.as_view(), name='pvp-iv'),
 ]
